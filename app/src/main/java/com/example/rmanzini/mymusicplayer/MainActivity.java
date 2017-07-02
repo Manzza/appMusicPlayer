0199f6beb7a02f8c9c4f3ba8cdbe4e0459058205
manzza <rmanzini@interaxa.com>
2017-07-02 14:10:00 -0300

    miwork starter + databind

--- app/src/main/java/com/example/rmanzini/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/rmanzini/mymusicplayer/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.rmanzini.mymusicplayer;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rmanzini.mymusicplayer.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+            MainActivityPresenter mainActivityPresenter = new MainActivityPresenter();
+            binding.setPresenter(mainActivityPresenter);
+
+        }
+
+
     }
-}
